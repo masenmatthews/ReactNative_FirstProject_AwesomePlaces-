d8b2f55fabae7050b161120901470384ab6d4364
Masen Matthews <masenmatthews@gmail.com>
2018-06-07 08:01:25 -0700

    successfully refactor code - now split into seperate components

--- App.js
+++ App.js
@@ -1,48 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import ListItem from './src/components/ListItem/ListItem';
+import { StyleSheet, View } from 'react-native';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends React.Component {
   state = {
-    placeName: '',
     places: []
   }
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
     });
-  }
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-  }
-  this.setState(prevState => {
-    return {
-      places: prevState.places.concat(prevState.placeName)
-    };
-  });
-};
+  };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-        placeholder="An awesome place"
-        value={this.state.placeName}
-        onChangeText={this.placeNameChangedHandler}
-        style={styles.placeInput}
-        />
-        <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler}/>
-        </View>
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
+        <PlaceInput onPlaceAdded = {this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -55,21 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-  },
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  },
-  listContainer: {
-    width: "100%",
   }
 });
